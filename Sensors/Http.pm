c7afa78a379d47930dd44f29a47beac4be0be820
Olivier Croquette <ocroquette@free.fr>
2013-06-04 10:56:32 +0200

    First useful version

--- Sensors/Http.pm
+++ Sensors/Http.pm
@@ -2,6 +2,7 @@ package Sensors::Http;
 
 use parent 'Sensors::Base';
 use strict;
+use IO::Socket::INET;
 
 sub new       {
   return bless {}, shift;
@@ -14,7 +15,23 @@ sub configure {
 
 sub do {
   my $self = shift;
-  $self->log("Http", "OK", $self->{host});
+  # $self->log("Http", "OK", $self->{host});
+  
+  my $socket = IO::Socket::INET->new(
+    PeerAddr => $self->{host},
+    PeerPort => 80,
+    Proto    => 'tcp',
+  ) or die("Error :: $!");
+
+  print($socket "GET / HTTP/1.1\n");
+  print($socket "Host: " . $self->{host} ."\n");
+  print($socket "\n");
+  my $received = <$socket>;
+  # print $received;
+
+  
+  my $status = ( $received =~ /^HTTP.... [23]/ ? "OK" : "NOK" );
+  $self->log("Http", $status, $self->{host});
 }
 
 1;
--- Sensors/Ping.pm
+++ Sensors/Ping.pm
@@ -2,6 +2,7 @@ package Sensors::Ping;
 
 use parent 'Sensors::Base';
 use strict;
+use File::Spec;
 
 sub new {
   return bless {}, shift;
@@ -14,7 +15,24 @@ sub configure {
 
 sub do {
   my $self = shift;
-  $self->log("Ping", "OK", $self->{host});
+  my $status = ( $self->check() ? "OK" : "NOK");
+  $self->log("Ping", $status, $self->{host});
+}
+
+sub check {
+  my $self = shift;
+  my @cmd = qw/ping -o -c 1 -t 2/;
+  push @cmd, $self->{host};
+
+  # Discard STDOUT
+  open my $saveout, ">&STDOUT";
+  open STDOUT, '>', File::Spec->devnull();
+
+  my $code = system(@cmd);
+
+  # Restore STDOUT
+  open STDOUT, ">&", $saveout;
+  return ($code == 0);
 }
 
 1;
